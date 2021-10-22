4191136fa21abbf19bc3ae77a1ae3f3c50aa8635
unknown <alexguldbrand@gmail.com>
2021-10-22 10:28:28 +0200

    wip

--- data.R
+++ data.R
@@ -49,7 +49,7 @@ carbonara_opskr <- tibble::tribble(
   "bacon i tern", 100, "gram"
 )
 
-fiskefilet_opsk <- tibble::tribble(
+fiskefilet_opskr <- tibble::tribble(
   ~"Fiskefilet", ~"maengde", ~"enhed",
   "fiskefilet", 1, "stk",
   "remoulade (tilbeh\u00F8r)", NA, "",
@@ -57,7 +57,7 @@ fiskefilet_opsk <- tibble::tribble(
   "kartofler", 0.5, "kg"
 )
 
-pizza_opsk <- tibble::tribble(
+pizza_opskr <- tibble::tribble(
   ~"Pizza", ~"maengde", ~"enhed",
   "tipo 00 mel", 125, "gram",
   "durummel", 25, "gram",
@@ -70,7 +70,78 @@ pizza_opsk <- tibble::tribble(
   "frisk mozzarella", 1, "stk"
 )
 
+burger_opskr <- tibble::tribble(
+  ~"Burger", ~"maengde", ~"enhed",
+  "hakket oksek\u00F8d", 0.175, "kg",
+  "bacon i skiver", 0.5, "pakke(r)",
+  "brioche bolle", 1, "stk",
+  "agurk", 0.2, "stk",
+  "iceberg", 0.1, "stk",
+  "toastost", 2, "skiver",
+  "ketchup (tilbeh\u00F8r)", NA, "",
+  "remoulade (tilbeh\u00F8r)", NA, "",
+  "ristet l\u00F8g (tilbeh\u00F8r)", NA, "",
+  "BBQ dressing (tilsmagning)", NA, "",
+  "eddike (tilsmagning)", NA, ""
+)
+
+frikadeller_opskr <- tibble::tribble(
+  ~"Frikadeller", ~"maengde", ~"enhed",
+)
+
+dahl_opskr <- tibble::tribble(
+  ~"dahl", ~"maengde", ~"enhed",
+)
+
+madpandekager_opskr <- tibble::tribble(
+  ~"madpandekager", ~"maengde", ~"enhed",
+)
+
+ravioli_opskr <- tibble::tribble(
+  ~"ravioli", ~"maengde", ~"enhed",
+)
+
+bacon_broccoli_Taerte_opskr <- tibble::tribble(
+  ~"", ~"maengde", ~"enhed",
+)
+
+Kyllingebowl_opskr <- tibble::tribble(
+  ~"kyllingebowl", ~"maengde", ~"enhed",
+)
+
+lasagne_opskr <- tibble::tribble(
+  ~"lasagne", ~"maengde", ~"enhed",
+)
+
+kalkunschnitzel_opskr <- tibble::tribble(
+  ~"kalkunschnitzel", ~"maengde", ~"enhed",
+)
+
+risengrod_opskr <- tibble::tribble(
+  ~"risengrod", ~"maengde", ~"enhed",
+)
+
+madpangekager_paa_panden_opskr <- tibble::tribble(
+  ~"madpangekager paa panden", ~"maengde", ~"enhed",
+)
+
+chili_con_carne_opskr <- tibble::tribble(
+  ~"chili con carne", ~"maengde", ~"enhed",
+)
+
+braendende_kaerlighed_opskr <- tibble::tribble(
+  ~"braendende kaerlighed", ~"maengde", ~"enhed",
+)
+
+kartoffel_porre_suppe_opskr <- tibble::tribble(
+  ~"kartoffel porre suppe", ~"maengde", ~"enhed",
+)
+
+
+
 opskrifter <- list(morbrad_opskr = morbrad_opskr,
                    carbonara_opskr = carbonara_opskr,
                    fiskefilet_opsk = fiskefilet_opsk,
-                   pizza_opsk = pizza_opsk)
+                   pizza_opsk = pizza_opsk,
+                   burger_opsk = burger_opsk)
+
